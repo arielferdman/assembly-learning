81e67e6d648b20f077093234f5a16610e18a57c2
ariel <arielxcon@gmail.com>
2019-08-05 15:21:11 +0000

    now compiled sandbox is the program to generate a file with random chars

--- sandbox.asm
+++ sandbox.asm
@@ -1,43 +1,42 @@
 section .bss
-	Buff resb 1
+	Buff resb 1			; reserve 1 byte in memory for current char to write to file
 section .data
+	counter dd 0			; this is the counter to know how many chars have already been written
 section .text
-	global _start
+	global _start			; define program entry point for linux to know
 
-_start:
-	nop
+_start:					; program entry point
+	  nop				; for the debugger
 
-Read:	mov eax,3 	; specify sys_read call
-	mov ebx,0	; specify file descriptor 0: standard input
-	mov ecx,Buff	; pass adress of the buffer to read to 
-	mov edx,1	; tell sys_read to read 1 char from stdin
-	int 80h		; call sys_read
+Generate: rdtsc 			; get cpu watch timestamp into edx:eax
+	  mov ecx,1bh			; move 27d to ecx
+	  mov edx,0			; zero out edx for div
+	  div ecx			; divide eax by 27d in ecx to get a number between 0-26 in edx (the modulo)
+					; the numbers 0-25 represent lower case characters and 26 represents a new line char
+		
+	  cmp edx,1ah			; compare the number in edx with 26d
+	  je Newline			; if equal jump to Newline label to write a newline char to stdout
+ 			
+	  add edx,61h			; add the value of edx by 97d (ascii char code of a) 
+					; since at this point 0<=edx<=25, the result of the addition is 97('a')<=edx<=122('z')
+	  mov [Buff],edx		; mov edx to memory at Buff
+	  jmp Write			; jump to Write label
+ 
+Newline:  mov byte [Buff],0ah		; move the ascii char code of a newline char to memory at Buff
+	  jmp Write			; jump to Write label
 
-	cmp eax,0	; look at sys_read return value in eax
-	je Exit		; jump if equal to 0 (EOF CHAR) to exit
-			; or fall through to test for lowercase
+Write:    mov eax,4			; specify sys_write system call
+	  mov ebx,1			; specify destination of sys_write to be file descriptor 1 (stdout)
+	  mov ecx,Buff			; move adress of memory of the byte to write to ecx
+	  mov edx,1			; specify that sys_write should write 1 character
+	  int 80h			; call sys_write
+	  inc dword [counter]		; increase the counter variable in memory to represent that another character was written
+	  cmp dword [counter],1000000	; compare counter variable content with 1000000
+	  ja Exit			; if counter passed that number then jump to Exit Label
+	  jmp Generate			; if not - jump to Generate label
 
-	cmp byte [Buff],61h ; test input char against lower case 'a'
-	jb Write	; if below 'a' in ASCII chart, not lowercase
-	
-	cmp byte [Buff],7Ah ; test input char against lower case 'z'
-	ja Write	; if above 'z' in ASCII char, not lowercase
-			; at this point we have a lowercase character
-	sub byte [Buff],20h ; substract 20h from lowercase to get uppercase
-			; and then write the char to stdout
-Write:	mov eax,4	; specify sys_write system call
-	mov ebx,1	; specify file descriptor 1 stdout
-	mov ecx,Buff	; pass address of the character to write
-	mov edx,1	; pass number of chars to write
-	int 80h		; call sys_write
-	jmp Read	; then go to the beggining to get another character
+Exit:	  mov eax,1			; specify sys_exit syscall
+	  mov ebx,0			; specify return code 0 (program terminated ok)
+	  int 80h			; call sys_exit
 
-Exit:	mov eax, 1	; code for Exit syscall
-	mov ebx, 0	; return code 0 (program terminated without errors)
-	int 80h		; make a kernel call to exit program
-
-
-
-	nop
-
-
+	  nop				; for the debugger
