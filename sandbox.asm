b345e23b7fc82e8b89fe976a143902e588686a2f
arielferdman <arielxcon@gmail.com>
2019-08-15 10:15:04 +0300

    progress with hexdumpext

--- sandbox.asm
+++ sandbox.asm
@@ -13,7 +13,8 @@ section .text
 _start:
 	nop
 	mov eax,OriginalStr
-	mov ebx,ORIGINALSTRLENGTH
+	mov ebx,Buff
+	mov ecx,HexStr
 
 Read:
 	mov eax,3
@@ -35,7 +36,9 @@ Scan:
 	mov al, byte [Buff + ecx]
 	
 	cmp al,0Ah
-	je  NewLine	
+	je  NewLine
+	cmp al,20h
+	je  Space	
 	mov byte [OriginalStr + edx + 2],al
 
 Resume:
@@ -67,15 +70,32 @@ Resume:
 NewLine:
 	mov byte [OriginalStr + edx + 1],5Ch
 	mov byte [OriginalStr + edx + 2],6Eh
+	jmp Resume
+
+Space:
+	mov byte [OriginalStr + edx + 1],5Ch
+	mov byte [OriginalStr + edx + 2],73h	
 	jmp Resume	
 	
 ZeroOriginalString:
-
-	mov byte [OriginalStr + ecx],20h
+	
+	mov edx,ecx
+	shl edx,1
+	add edx,ecx
+	mov byte [OriginalStr + edx],20h	
+	mov byte [OriginalStr + edx+1],20h
+	mov byte [OriginalStr + edx+2],20h
+	mov byte [HexStr + edx],20h
+	mov byte [HexStr + edx + 1],30h
+	mov byte [HexStr + edx + 2],30h
 	inc ecx
-	cmp ecx,ORIGINALSTRLENGTH
+	cmp ecx,BUFFLEN
 	jb ZeroOriginalString
-	mov byte [OriginalStr + ecx],0Ah
+	mov edx,ecx
+	shl edx,1
+	add edx,ecx
+	mov byte [OriginalStr + edx],0Ah
+	mov byte [HexStr + edx],0Ah
 	jmp Read
 
 Exit:
